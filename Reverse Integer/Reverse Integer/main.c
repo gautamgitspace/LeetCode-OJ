2add10666016438a296047eb496ee3bb6dc76efe
Abhishek Gautam <agautam2@buffalo.edu>
2018-07-11 12:19:02 -0400

    accepted solution with min max macros

--- Reverse Integer/Reverse Integer/main.c
+++ Reverse Integer/Reverse Integer/main.c
@@ -1,37 +1,28 @@
-// Reverse an Integer. LeetCode OJ Problem #7
-
 #include <stdio.h>
+#DEFINE INT_MAX 2147483647
+#DEFINE INT_MIN -2147483648
 
 
-int reverseInteger(int number)
+long reverse(long number)
 {
-    int result = 0;
+    long result = 0;
     int flag = 0;
     if (number < 0)
     {
         number = 0 - number;
         flag = 1;
-        printf("Making Number Positive: %d\n", number);
     }
     int p = number;
     while(p > 0)
     {
-        /*CALCULATE VALUE TO APPEND AT EACH LEVEL*/
-        int mod = p % 10;
-        /*REDUCE P*/
-        p = p/10;
-        /*APPEND VALUE TO RESULT, SHIFT USING MULTIPLICATION BY 10*/
-        result = result * 10 + mod;
+        result = result * 10 + p % 10;
+        p = p / 10;
     }
         if(flag == 1)
         {
             result = 0 - result;
         }
+    if (result > INT_MAX || result < INT_MIN)
+        return 0;
     return result;
-}
-
-int main(int argc, const char * argv[])
-{
-    printf("Reverse: %d\n", reverseInteger(-123));
-    return 0;
 }
