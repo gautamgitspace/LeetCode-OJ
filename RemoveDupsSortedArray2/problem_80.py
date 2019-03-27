bdb23364502e7babd1e51c40944485f005d2e8af
gautamgitspace <agautam2@buffalo.edu>
2019-03-27 16:13:37 -0700

    #80 Only allow one copy of Dups and remove others, return len

---
+++ RemoveDupsSortedArray2/problem_80.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python
+
+"""
+[1,1,2,2,3,3,3,4]  becomes [1,1,2,2,3,3,4] and returns lengh 7
+
+* key -> sorted, so numbers won't appear our of order
+* clause -> CANNOT allocate memory to new lists/arrays
+* how -> keep scanning subsequent occurring indices and if exceeds count of 2, i.e. seen for the 3rd time, move it to the last.
+"""
+
+def _prune (arr):
+    count = 1
+    length = len(arr)
+    i = 1
+    while i < length:
+        if arr[i] == arr[i-1]:
+            print str(arr[i]) + " matches " + str(arr[i-1])
+            count += 1
+        else:
+            count = 1
+        if count > 2:
+            del arr[i]
+            length -= 1
+        else:
+            i += 1
+    return len(arr), arr
+
+arr = [1,1,1,2,2,3,3,4,4,4,4]
+print _prune(arr)
