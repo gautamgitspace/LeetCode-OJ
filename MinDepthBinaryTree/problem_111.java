ccb274257dc0b0d48d6f8d0680d87ffeecaed974
gautamgitspace <agautam2@buffalo.edu>
2019-05-18 21:13:10 -0700

    + MaxDepth as well

--- MinDepthBinaryTree/problem_111.java
+++ MinDepthBinaryTree/problem_111.java
@@ -5,4 +5,10 @@ class Solution {
         int right_depth = minDepth(root.right);
         return (left_depth == 0 || right_depth ==0) ? left_depth + right_depth + 1 : Math.min(left_depth, right_depth) + 1;
     }
+    public int maxDepth(TreeNode root) {
+      if (root == null) return 0;
+      int left_depth = maxDepth(root.left);
+      int right_depth = maxDepth(root.right);
+      return Math.max(left_depth, right_depth) + 1
+    }
 }
