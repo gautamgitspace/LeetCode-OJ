519067e7579f2bf31a197cf2b1c239906532876e
gautamgitspace <agautam2@buffalo.edu>
2019-05-27 22:13:06 -0700

    list has cycle two - return the node

---
+++ LinkedListCycle/has_cycle_two.java
@@ -0,0 +1,24 @@
+public class Solution {
+    public ListNode detectCycle(ListNode head) {
+        ListNode slow = head;
+        ListNode fast = head;
+
+        while (fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+            if (slow == fast) {
+                break;
+            }
+        }
+
+        if (fast == null || fast.next == null) {
+            return null;
+        }
+        slow = head;
+        while (slow != fast) {
+            slow = slow.next;
+            fast = fast.next;
+        }
+        return fast;
+    }
+}
