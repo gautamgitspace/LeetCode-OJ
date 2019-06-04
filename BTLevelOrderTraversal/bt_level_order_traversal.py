965fd74afc5cdaae515951b86c9725e6c8f97ff6
gautamgitspace <agautam2@buffalo.edu>
2019-06-03 22:50:36 -0700

    return reverse order 🤦🏻‍♂️

--- BTLevelOrderTraversal/bt_level_order_traversal.py
+++ BTLevelOrderTraversal/bt_level_order_traversal.py
@@ -26,3 +26,25 @@ class Solution(object):
             level = next_level
 
         return result
+
+    def levelOrderReverse(self, root):
+        """
+        :type root: TreeNode
+        :rtype: List[List[int]]
+        """
+        result = []
+        level = [root]
+
+        while root and level:
+            current_nodes = []
+            next_level = []
+            for node in level:
+                current_nodes.append(node.val)
+                if node.left:
+                    next_level.append(node.left)
+                if node.right:
+                    next_level.append(node.right)
+            result.append(current_nodes)
+            level = next_level
+
+        return result[::-1]
