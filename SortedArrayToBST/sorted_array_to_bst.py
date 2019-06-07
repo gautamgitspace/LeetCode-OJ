b7decff6ae78898d0446a7f2ad1dc761fa1f53bb
gautamgitspace <agautam2@buffalo.edu>
2019-06-07 11:55:44 -0700

    Sorted array to BST

---
+++ SortedArrayToBST/sorted_array_to_bst.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python
+"""
+KEY - BST mid logic, keep on constructing left and right using the new mid
+"""
+class Solution(object):
+    def sortedArrayToBST(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: TreeNode
+        """
+        if not nums:
+            return None
+        mid = len(nums) // 2
+        root = TreeNode(nums[mid])
+
+        root.left = self.sortedArrayToBST(nums[:mid])
+        root.right = self.sortedArrayToBST(nums[mid+1:])
+
+        return root
