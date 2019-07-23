272af5bc314722c5dfe08986c6922c7b75e87def
gautamgitspace <agautam2@buffalo.edu>
2019-07-23 00:35:35 -0700

    BSTfromPreOrder/bst_from_pre_order.py

---
+++ BSTfromPreOrder/bst_from_pre_order.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python
+'''
+Gen BST from given list of preorder
+
+KEY is to start with having root in place which is the
+first element of preorder. after that we have to find
+the pivot index (root), towards the left of which all
+elements are smaller and towards the right of which all
+elements are bigger.
+
+i.e if (preorder[i] > root.val)
+'''
+class Solution(object):
+    def bstFromPreorder(self, preorder):
+        """
+        :type preorder: List[int]
+        :rtype: TreeNode
+        """
+        if not preorder:
+            return None
+        node = TreeNode(preorder[0])
+        i = 0
+        for i in range(len(preorder)):
+            if preorder[i] > node.val:
+                break
+            i += 1
+
+        node.left = self.bstFromPreorder(preorder[1:i])
+        node.right = self.bstFromPreorder(preorder[i:])
+        return node
