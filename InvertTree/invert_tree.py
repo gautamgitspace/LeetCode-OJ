641387a4596788e6966317f66cd06a3e3899a867
gautamgitspace <agautam2@buffalo.edu>
2019-07-24 22:35:20 -0700

    Invert tree or Homebrew?

---
+++ InvertTree/invert_tree.py
@@ -0,0 +1,8 @@
+#!/usr/bin/pyhon
+
+class Solution(object):
+    def invertTree(self, root):
+        if not root:
+            return None
+        root.left, root.right = self.invertTree(root.right), self.invertTree(root.left)
+        return root
