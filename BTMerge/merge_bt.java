b8600d10053cdd3c043cc25e5214d21c3526cab6
gautamgitspace <agautam2@buffalo.edu>
2019-09-10 12:53:18 -0700

    merge two BTs (add values of nodes to form resultant node)

---
+++ BTMerge/merge_bt.java
@@ -0,0 +1,13 @@
+class Solution {
+    public TreeNode mergeTrees(TreeNode t1, TreeNode t2) {
+        if (t1 == null && t2 == null) return null;
+        int val = (t1 == null ? 0 : t1.val) + (t2 == null ? 0 : t2.val);
+        TreeNode t3 = new TreeNode(val);
+        // till this point, we have fabricated the root
+
+        t3.left = mergeTrees(t1 == null ? null : t1.left, t2 == null ? null : t2.left);
+        t3.right = mergeTrees(t1 == null ? null : t1.right, t2 == null ? null : t2.right);
+
+        return t3;
+    }
+}
---
+++ BTMerge/merge_bt.py
@@ -0,0 +1,14 @@
+class Solution(object):
+    def mergeTrees(self, t1, t2):
+        """
+        :type t1: TreeNode
+        :type t2: TreeNode
+        :rtype: TreeNode
+        """
+        if not t1 and not t2: return None
+        elif not t1: return t2
+        elif not t2: return t1
+        t3 = TreeNode(t1.val + t2.val)
+        t3.left = self.mergeTrees(t1.left, t2.left)
+        t3.right = self.mergeTrees(t1.right, t2.right)
+        return t3
