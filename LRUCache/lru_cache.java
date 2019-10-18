de4c84b93a4cec8a43baa6cf7eb5549cbf3ccc60
gautamgitspace <agautam2@buffalo.edu>
2019-10-18 00:58:29 -0700

    HT + DLL. eh?

---
+++ LRUCache/lru_cache.java
@@ -0,0 +1,96 @@
+class LRUCache {
+  // Hashtable backs up the Doubly Linked List for O(1) access to cache items
+  Map<Integer, ListNode> hashtable = new HashMap<Integer, ListNode>();
+  ListNode head;
+  ListNode tail;
+
+  int totalItemsInCache;
+  int maxCapacity;
+
+  public LRUCache(int maxCapacity) {
+    totalItemsInCache = 0;
+    this.maxCapacity = maxCapacity;
+
+    // Dummy head and tail nodes to avoid empty states
+    head = new ListNode();
+    tail = new ListNode();
+
+    // Wire the head and tail together
+    head.next = tail;
+    tail.prev = head;
+  }
+
+  public int get(int key) {
+    ListNode node = hashtable.get(key);
+    if (node == null) return -1;
+    // else Item exists and has been accessed.
+    // Move the item to the front of the cache
+    moveToHead(node);
+    return node.value;
+  }
+
+  public void put(int key, int value) {
+    // fetch node at key from hash table
+    ListNode node = hashtable.get(key);
+
+    if (node == null) {
+      // Item not found, create a new entry
+      ListNode newNode = new ListNode();
+      newNode.key = key;
+      newNode.value = value;
+
+      // Add to the hashtable and the actual list that represents the cache
+      hashtable.put(key, newNode);
+      addToFront(newNode);
+      totalItemsInCache++;
+
+      // If over capacity remove the LRU item
+      if (totalItemsInCache > maxCapacity) removeLRUEntry();
+    } else {
+      // item is found in the cache, we update it
+      // and we move it to the head of the list
+      node.value = value;
+      moveToHead(node);
+    }
+  }
+
+  private void removeLRUEntry() {
+    ListNode tail = popTail();
+    hashtable.remove(tail.key);
+    --totalItemsInCache;
+  }
+
+  private ListNode popTail() {
+    ListNode tailItem = tail.prev;
+    removeFromList(tailItem);
+    return tailItem;
+  }
+
+  private void addToFront(ListNode node) {
+    // Wire up the new node being to be inserted
+    node.prev = head;       // link node backwards to head
+    node.next = head.next;  // link nodes forward to head's next
+    head.next.prev = node;  // link head's next backwards to node
+    head.next = node;       // finally update head's next to be this node
+  }
+
+  private void removeFromList(ListNode node) {
+    ListNode savedPrev = node.prev;
+    ListNode savedNext = node.next;
+    savedPrev.next = savedNext;
+    savedNext.prev = savedPrev;
+  }
+
+  private void moveToHead(ListNode node) {
+    removeFromList(node);
+    addToFront(node);
+  }
+
+  private class ListNode {
+    int key;
+    int value;
+
+    ListNode prev;
+    ListNode next;
+  }
+}
